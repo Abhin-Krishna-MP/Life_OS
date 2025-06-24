4667c53e04f0b64c46d4b91a489711f042fd8fcb
Abhin_krishna <abhinkrishnamp0@gmail.com>
2025-06-24 09:37:44 +0530

    minor changes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ChallengeViewSet, JournalViewSet, UserProfileViewSet, mark_streak_day, get_user_profile
+from .views import ChallengeViewSet, JournalViewSet, UserProfileViewSet, mark_streak_day, get_user_profile, run_migrations
 
 router = DefaultRouter()
 
@@ -12,4 +12,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('profile/', get_user_profile),
     path('challenges/<int:challenge_id>/mark-day/', mark_streak_day),
+    path('run-migrations/', run_migrations),
+
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -14,6 +14,14 @@ from .serializers import ChallengeSerializer, JournalEntrySerializer, UserProfil
 
 
 # Create your views here.
+@api_view(['POST'])
+def run_migrations(request):
+    try:
+        call_command('makemigrations', 'core')  # Optional, only needed if you changed models
+        call_command('migrate')
+        return Response({'message': 'Migrations applied successfully'})
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
